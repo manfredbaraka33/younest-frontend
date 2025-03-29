c3b0b62c2585e2cbec27fa3b05a8d135e0f068f6
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-03-29 17:29:22 +0300

    Update api.js

--- younest/src/services/api.js
+++ younest/src/services/api.js
@@ -1,13 +1,13 @@
 
 export const getProductsOrServices = async () => {
-  const response = await fetch("http://localhost:8000/api/fetch_all_pos/");
+  const response = await fetch("http://13.60.222.132/api/fetch_all_pos/");
   const data = await response.json();
   console.log(data.products);
   return data.products;
 };
 
 export const getShops = async () => {
-  const response = await fetch("http://localhost:8000/api/shops/");
+  const response = await fetch("http://13.60.222.132/api/shops/");
   const data = await response.json();
   console.log(data.results);
   return data.results;
@@ -15,7 +15,7 @@ export const getShops = async () => {
 
 
 export const getFosSale = async () => {
-  const response = await fetch("http://localhost:8000/api/forsale2/");
+  const response = await fetch("http://13.60.222.132/api/forsale2/");
   const data = await response.json();
   console.log(data.products);
   return data.products;
