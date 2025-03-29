5a079872d422a879fc5751193e980c3b0deff00f
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-03-29 17:42:32 +0300

    Update axios.js

--- younest/src/helpers/axios.js
+++ younest/src/helpers/axios.js
@@ -4,7 +4,7 @@ import { getAccessToken, getRefreshToken, getUser } from "../hooks/user.actions"
 
 // Create an Axios instance with default settings
 const axiosService = axios.create({
-  baseURL: "http://13.60.222.132/api", // Update with your API's base URL
+  baseURL: "https://13.60.222.132/api", // Update with your API's base URL
   headers: {
     "Content-Type": "multipart/form-data",
   },
@@ -28,7 +28,7 @@ const refreshAuthLogic = async (failedRequest) => {
         refresh: getRefreshToken(),
       },
       {
-        baseURL: "http://13.60.222.132/api", // Same API base URL
+        baseURL: "https://13.60.222.132/api", // Same API base URL
       }
     );
 
