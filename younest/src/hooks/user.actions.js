d9c8ffc6b297dfcbd82e1b6441c940119ea6ace0
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-03-29 17:41:33 +0300

    Update user.actions.js

--- younest/src/hooks/user.actions.js
+++ younest/src/hooks/user.actions.js
@@ -3,7 +3,7 @@ import axiosService from "../helpers/axios"; // Make sure axiosService handles a
 import axios from "axios";
 
 // Your base URL for API calls
-const baseURL = "http://localhost:8000/api";
+const baseURL = "https://13.60.222.132/api/";
 
 function useUserActions() {
   const navigate = useNavigate();
