2bd3fd38255e11629a1e929ce1de34fd0050584a
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-03-29 17:44:55 +0300

    Update SavedPosCard.jsx

--- younest/src/components/SavedPosCard.jsx
+++ younest/src/components/SavedPosCard.jsx
@@ -69,7 +69,7 @@ const SavedPosCard = ({p}) => {
 
         {/* Check if there's at least one image */}
         {images.length > 0 ? (
-            <img src={"http://127.0.0.1:8000"+images[currentImageIndex]?.image} alt={p.name} />
+            <img src={"https://13.60.222.132"+images[currentImageIndex]?.image} alt={p.name} />
         ) : (
             <img src="default-image.jpg" alt="default" /> // Fallback if no images
         )}
