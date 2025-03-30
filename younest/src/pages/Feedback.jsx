e2b06e0baa2c5117ffe22cbd04657b7f3c868d77
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-03-30 16:01:50 +0300

    Update Feedback.jsx

--- younest/src/pages/Feedback.jsx
+++ younest/src/pages/Feedback.jsx
@@ -10,7 +10,7 @@ const Feedback = () => {
     const handleSubmit=async (e)=>{
         e.preventDefault(); 
         try{
-            const response = await fetch('http://localhost:8000/api/feedback/',
+            const response = await fetch('https://younestapi.publicvm.com/api/feedback/',
                 {
                     method:"POST",
                     headers:{
