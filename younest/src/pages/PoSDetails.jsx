94492d9611a2418775e3721073d8fffea4848c80
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-03-30 16:05:00 +0300

    Update PoSDetails.jsx

--- younest/src/pages/PoSDetails.jsx
+++ younest/src/pages/PoSDetails.jsx
@@ -40,7 +40,7 @@ const PoSDetails = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const getDetails = async () => {
-    const response = await fetch(`https://13.60.222.132/api/pos/${posId}/`);
+    const response = await fetch(`https://younestapi.publicvm.com/api/pos/${posId}/`);
     const data = await response.json();
     return data;
   };
