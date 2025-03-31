a3e8a63dd016ad07864da90d6ee8a7ac3221a018
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-03-31 12:37:12 +0300

    Update MyShops.jsx

--- younest/src/pages/MyShops.jsx
+++ younest/src/pages/MyShops.jsx
@@ -9,15 +9,21 @@ const MyShops = () => {
   const [shops, setShops] = useState([]);
   const { user } = useAuth(); // Access the user from AuthContext
   const navigate = useNavigate();
+  const [loading,setLoading]=useState(false);
+  const [alert,setAlert]=useState(false);
   
  
 useEffect(() => {
   const fetchShops = async () => {
     try {
+      setLoding(true);
       const response = await getData('/user-shops/'); // Await the API call
       console.log(response);
+      setLoading(false);
       setShops(response); // response.data is not needed since getData already returns response.data
     } catch (error) {
+      setLoading(false);
+      setAlert(true);
       console.error('Error fetching shops:', error);
     }
   };
@@ -42,7 +48,16 @@ useEffect(() => {
         </div>
       </div>
       <div className='container'>
-        {shops?.length > 0 ? (
+        {loading ? (<div className="loading d-flex align-items-center">
+          
+              <div className="spinner-border spinner-border-sm" role="status">
+                <span className="sr-only"></span>
+              </div>
+              <span className="ms-2">Loading data...</span>
+      
+        </div>):(
+          <>
+          {shops?.length > 0 ? (
           shops.map(shop => (
             <Link key={shop.id} style={{ textDecoration: "none" }} to={`/shop/${shop.id}`}>
               <div className='row border border-secondary p-2 rounded my-2'>
@@ -62,6 +77,8 @@ useEffect(() => {
           <div classname="mt-3"><p >You have no Bussineses yet. Click <Link to="/addshop">here</Link> to add a business.</p>
           <p style={{color:"red"}}>If you have created business still this is showing try to log out and <Link to="/login">login</Link> again</p>
           </div>
+        )}
+          </>
         )}
       </div>
     </div>
