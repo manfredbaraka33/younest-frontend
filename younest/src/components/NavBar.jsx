9e9f426b46295e8ced00a4ed1ba2f034a6034274
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-03-31 11:29:33 +0300

    Update NavBar.jsx

--- younest/src/components/NavBar.jsx
+++ younest/src/components/NavBar.jsx
@@ -9,7 +9,7 @@ import NotificationBell from "./NotificationBell";
 function NavBar({ toggleSidebar, setIsSidebarOpen }) {
   const { user } = useAuth();
   const nav = useNavigate();
-  if(user?.profile_image === null){
+  if(!user?.profile_image){
     window.alert("Token Expired login again!");
     nav("/login");
   }
