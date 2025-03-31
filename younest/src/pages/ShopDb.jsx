406dc792ef7a211b55883480055e2ffaefa1c12d
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-03-31 18:32:10 +0300

    Update ShopDb.jsx

--- younest/src/pages/ShopDb.jsx
+++ younest/src/pages/ShopDb.jsx
@@ -6,7 +6,7 @@ import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Toolti
      
 // Register necessary chart elements
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement);
-   
+       
 const ShopDb = () => { 
     const { shopId } = useParams();
     const [shopData, setShopData] = useState(null);
