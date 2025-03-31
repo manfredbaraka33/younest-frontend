e6f65a60fb8cb24e35b23ca84bf00d225c462fff
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-03-31 17:14:07 +0300

    Update PoSDetails.jsx

--- younest/src/pages/PoSDetails.jsx
+++ younest/src/pages/PoSDetails.jsx
@@ -200,6 +200,7 @@ const PoSDetails = () => {
             <div className="col my-2">
               <center>
               <PoSCard2 p={product} />
+                <span className="sm">Double tap image to enter or exit full-screen mode</span>
               </center>
             </div>
             <div className="col my-2">
